3068b1d94ee8b33250b3e099731bb6a57790a878
Prajna J k <acharyaprajna642@gmail.com>
2025-07-05 21:47:17 +0530

    appsettings added

--- Todo.DalLayer/Models/TodoTrackerDbContext.cs
+++ Todo.DalLayer/Models/TodoTrackerDbContext.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
 
 namespace Todo.DalLayer.Models;
 
@@ -21,7 +22,11 @@ public partial class TodoTrackerDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source =(localdb)\\MSSQLLocalDB;Initial Catalog=TodoTrackerDB;Integrated Security=true");
+       => optionsBuilder.UseSqlServer(
+        new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
